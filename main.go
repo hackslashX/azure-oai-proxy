3bbaf010b1868666c674d1318ecf6dd33c35abdf
Gyarbij (Chono N) <49493993+Gyarbij@users.noreply.github.com>
2024-06-22 21:29:14 +0200

    Update model list for full azure openai support
    GPT-3.5 Turbo:
    
    gpt-35-turbo-0301
    gpt-35-turbo-0613
    gpt-35-turbo-1106
    gpt-35-turbo-0125
    gpt-35-turbo-16k
    GPT-4:
    
    gpt-4-0613
    gpt-4-1106-preview
    gpt-4-0125-preview
    gpt-4-vision-preview
    gpt-4-turbo-2024-04-09
    gpt-4o-2024-05-13
    gpt-4-32k-0613
    Embeddings:
    
    text-embedding-3-large
    text-embedding-3-small
    text-embedding-ada-002
    DALL-E:
    
    dall-e-2
    dall-e-3
    Other models:
    
    babbage-002
    davinci-002
    whisper-001

--- main.go
+++ main.go
@@ -63,7 +63,13 @@ func main() {
 }
 
 func handleGetModels(c *gin.Context) {
-	models := []string{"gpt-4o", "gpt-4", "gpt-4-32k", "gpt-4-vision-preview", "gpt-3.5-turbo", "gpt-4-turbo", "dall-e-3", "text-embedding-ada-002"}
+	models := []string{
+		"gpt-4o", "gpt-4-turbo", "gpt-4", "gpt-4o-2024-05-13", "gpt-4-turbo-2024-04-09", "gpt-4-0613", "gpt-4-1106-preview", "gpt-4-0125-preview", "gpt-4-vision-preview", "gpt-4-32k-0613",
+		"gpt-35-turbo-0301", "gpt-35-turbo-0613", "gpt-35-turbo-1106", "gpt-35-turbo-0125", "gpt-35-turbo-16k",
+		"text-embedding-3-large", "text-embedding-3-small", "text-embedding-ada-002",
+		"dall-e-2", "dall-e-3",
+		"babbage-002", "davinci-002", "whisper-001",
+	}
 	result := azure.ListModelResponse{
 		Object: "list",
 	}
